a97213de1907f31aa4a008b2f10213dabb028946
Rohan Harugop <rohan.harugop@gmail.com>
2024-09-03 16:28:07 +0530

    navbar update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import HorizontalNav from "./components/navBarStuff/HorizontalNav";
 function App() {
   return (
     <div>
+      <HorizontalNav/>
       <h1>Hello World Rohan!</h1>
     </div>
   );
