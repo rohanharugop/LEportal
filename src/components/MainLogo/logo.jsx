b4535e2ef2eed2567c38901ad50430b8d9e9029b
Rohan Harugop <rohan.harugop@gmail.com>
2024-09-04 19:44:11 +0530

    logo again

--- src/components/MainLogo/logo.jsx
+++ src/components/MainLogo/logo.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import './logo.css';
+import img1 from './images/logo.png';  // Corrected import path
 
 const MainLogo = () => {
     return (
         <div id="logo">
-             <img src='/logo.png' alt="Main Logo" />
+            <img src={img1} alt="Main Logo" />  {/* Use the imported image variable */}
         </div>
     );
 }
