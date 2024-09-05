f6f8471fd0fbc5cd2c9681869c1b80f575064dea
Rohan Harugop <rohan.harugop@gmail.com>
2024-09-05 11:18:55 +0530

    logo positioned correctly

--- src/components/MainLogo/logo.jsx
+++ src/components/MainLogo/logo.jsx
@@ -4,8 +4,8 @@ import img1 from './images/logo.png';  // Corrected import path
 
 const MainLogo = () => {
     return (
-        <div id="logo">
-            <img src={img1} alt="Main Logo" />  {/* Use the imported image variable */}
+        <div id="logo-div">
+            <img id = 'logo-div img'src={img1} alt="Main Logo" />  {/* Use the imported image variable */}
         </div>
     );
 }
