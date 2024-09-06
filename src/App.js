f0adf3f33e571bad36b167162c8506e8397237be
Rohan Harugop <rohan.harugop@gmail.com>
2024-09-06 13:03:42 +0530

    about page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import React Router components
 import HorizontalNav from "./components/navBarStuff/HorizontalNav";
 import MainLogo from "./components/MainLogo/logo";
-// Testing new branch
-
+// import HomePage from "./components/HomePage/HomePage"; // Home page component
+import AboutPage from "./components/navBarStuff/AboutPage";
 function App() {
   return (
-    <div>
-      <div id="logo-div">
-        <MainLogo />
+    <Router>
+      <div>
+        <div id="logo-div">
+          <MainLogo />
+        </div>
+        <div id="navbar" className="navbar-padding">
+          <HorizontalNav />
+        </div>
+
+        {/* Define routes for different pages */}
+        <Routes>
+          {/* <Route path="/" element={<HomePage />} /> Home page */}
+          <Route path="/about" element={<AboutPage />} /> {/* About Us page */}
+        </Routes>
       </div>
-      <div id="navbar" className="navbar-padding">
-        <HorizontalNav />
-      </div>
-    </div>
+    </Router>
   );
 }
 
